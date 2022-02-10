6fbd67b201c9f8861308e1bc2b3153271a12fd4d
muhdsalm <73386616+muhdsalm@users.noreply.github.com>
2022-02-10 16:51:12 +0500

    Made SFML project
    project restart

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 #include <SDL2/SDL_image.h>
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
+#include <time.h>
+#include <chrono>
+#include <cstdint>
 
 #include "RenderWindow.hpp"
 #include "Entity.hpp"
@@ -11,13 +15,16 @@
 
 enum GameState
 {
-	main_menu, help_menu_controls, help_menu_settings, help_menu_info
+	main_menu, help_menu_controls, help_menu_settings, help_menu_info, my_station, play
 };
 
 int main(int argc, char* args[])
 {
+	srand(time(NULL));
 	std::string imageprefix = "_small.png";
 	Vector2f windowsize(721, 700 	);
+	int backgrounds[9][9] = {};
+	Entity backtex;
 	/*{
 		SDL_DisplayMode DM;
 		SDL_GetCurrentDisplayMode(-1, &DM);
@@ -41,6 +48,9 @@ int main(int argc, char* args[])
 	Entity help_page_controls(Vector2f(0, 0), window.loadTexture( ("res/UI/Help UI/controls_page" + imageprefix).c_str() ), Vector2f(windowsize.x, windowsize.y));
 	Entity help_page_settings(Vector2f(0, 0), window.loadTexture( ("res/UI/Help UI/settings_page" + imageprefix).c_str() ), Vector2f(windowsize.x, windowsize.y));
 	Entity help_page_info(Vector2f(0, 0), window.loadTexture( ("res/UI/Help UI/info_page" + imageprefix).c_str() ), Vector2f(windowsize.x, windowsize.y));
+	Entity my_station_page(Vector2f(0, 0), window.loadTexture( ("res/UI/my station UI/my_station_UI" + imageprefix).c_str() ), Vector2f(windowsize.x, windowsize.y));
+
+	Entity space(Vector2f(0, 0), window.loadTexture("res/in game background/space.png"), Vector2f(80, 80));
 
 
 	bool gameRunning = true;
@@ -51,6 +61,8 @@ int main(int argc, char* args[])
 
 	while (gameRunning)
 	{
+		int then = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+
 		// Get our controls and events
 		while (SDL_PollEvent(&event))
 		{
@@ -70,11 +82,27 @@ int main(int argc, char* args[])
 					//helpbuttonheight = 370, 415
 					std::cout << "You clicked!" << std::endl;
 					if(event.motion.x > 325 && event.motion.x < 415){
-						if(event.motion.y > 305 && event.motion.y < 352){
-							std::cout << "You pressed the button!";
+						if(event.motion.y > 277 && event.motion.y < 323){
+
+							for(int i = 0; i < 9; i++){
+								for(int j = 0; j < 9; j++){
+									backgrounds[i][j] = rand() % 4 + 1;
+								}
+							}
+							gameState = play;
 						}
 						if(event.motion.y > 370 && event.motion.y < 415){
 							gameState = help_menu_controls;
+						}
+						if(event.motion.y > 460 && event.motion.y < 508){
+							gameRunning = false;
+							std::cout << "Program Burst :(";
+						}
+					}
+					if(event.motion.x > 546 && event.motion.x < 703){
+						if(event.motion.y > 643 && event.motion.y < 690){
+							gameState = my_station;
+							std::cout << "It worked!";
 						}
 					}
 				}
@@ -112,6 +140,15 @@ int main(int argc, char* args[])
 				}
 				
 			}
+			if(gameState == my_station){
+				if(event.button.button == SDL_BUTTON_LEFT){
+					if(event.motion.x > 620 && event.motion.x < 720){
+						if(event.motion.y > 657 && event.motion.y < 700){
+							gameState = main_menu;
+						}
+					}
+				}
+			}
 		}
 
 		window.clear();
@@ -134,10 +171,27 @@ int main(int argc, char* args[])
 		if(gameState == help_menu_info){
 			window.render(help_page_info);
 		}
+		if(gameState == my_station){
+			window.render(my_station_page);
+		}
 		//std::cout << gameState << std::endl;
+		if(gameState == play){
+			space = NULL;
+			for(int i = 0; i < 9; i++){
+				for(int j = 0; j < 9; j++){
+					space = Entity(Vector2f(i * 80, j * 80), window.loadTexture(("res/in game background/space" + std::to_string(backgrounds[i][j]) + ".png").c_str()), Vector2f(80, 80));
+					window.render(space);
+					//return 1;
+				}
+			}
+		}
 
-
+		
 		window.display();
+
+		int now = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+		SDL_Delay(17);
+		
 
 	}
 
